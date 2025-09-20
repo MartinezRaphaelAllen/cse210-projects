81b3647d76f9c7adf06abf96df3cb9dc34e2a659
MartinezRaphaelAllen <mar24177@byui.edu>
2025-09-20 15:27:26 -0600

    Week 1 Prove assignments.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        string letter;
+        Console.Write("What is the percentage of your grade? ");
+        int grade = int.Parse(Console.ReadLine());
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade letter is {letter}");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed.");
+        }
+        else
+        {
+            Console.WriteLine("You failed. But that's okay, everyone grows from their mistakes.");
+        }
     }
 }
