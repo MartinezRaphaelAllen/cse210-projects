90bc165e1b617ed03bad281e75f16dd0621a1c25
MartinezRaphaelAllen <mar24177@byui.edu>
2025-11-11 17:13:25 -0700

    Prepare assignment week 9

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(new Square("red", 5));
+        shapes.Add(new Rectangle("purple", 10, 4));
+        shapes.Add(new Circle("yellow", 3.5));
+
+        foreach (Shape s in shapes)
+        {
+            Console.WriteLine(s.GetColor());
+            Console.WriteLine(s.GetArea());
+        }
     }
 }
